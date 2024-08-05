5a14b2a11eacd56bb77695299f080051d5e416d8
arunnats <arun_b220019cs@nitc.ac.in>
2024-08-06 00:11:49 +0530

    Pretty much done with frontend

--- github-glazer/src/App.tsx
+++ github-glazer/src/App.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 const App: React.FC = () => {
 	const [username, setUsername] = useState("");
 	const [response, setResponse] = useState("");
+	const [responseReceived, setResponseReceived] = useState(false);
 
 	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setUsername(event.target.value);
@@ -10,34 +11,32 @@ const App: React.FC = () => {
 
 	const handleButtonClick = () => {
 		console.log("GitHub Username:", username);
-		// Example response based on the username
 		setResponse(`You entered: ${username}`);
+		setResponseReceived(true);
 	};
 
 	return (
-		<div className="flex flex-col items-center min-h-screen p-4">
-			<h1 className="text-4xl font-bold m-4">GitHub Glazers</h1>
-			<p className="text-lg mb-4">Enter a GitHub username to start glazing</p>
+		<div className="flex flex-col items-center min-h-screen p-8">
+			<h1 className="text-4xl font-bold mb-6 text-center">GitHub Glazers</h1>
+			<p className="text-2xl mb-6">Enter a GitHub username to start glazing</p>
 			<input
 				type="text"
 				placeholder="Enter GitHub username"
 				value={username}
 				onChange={handleInputChange}
-				className="input bg-primary input-bordered w-full max-w-xs mb-4"
+				className="input input-bordered bg-primary w-full max-w-md text-xl p-4 mb-6"
 			/>
 			<button
 				onClick={handleButtonClick}
-				className="btn bg-primary p-2 rounded-md w-full max-w-xs mb-4"
+				className="btn bg-primary w-full max-w-md mb-6 text-xl"
 			>
 				Glaze
 			</button>
-			<div className="my-auto w-full max-w-s">
-				{response ? (
-					<div className="text-lg font-bold p-4 rounded-md w-full max-w-xs text-white bg-primary">
-						{response}
-					</div>
+			<div className="flex flex-col w-full max-w-lg min-h-[200px] bg-primary text-white p-6 rounded-md">
+				{responseReceived ? (
+					<div className="text-xl font-bold">{response}</div>
 				) : (
-					<div className="text-lg font-bold p-4 rounded-md w-full max-w-xs text-white bg-primary">
+					<div className="text-xl font-bold">
 						Enter a username to start glazing
 					</div>
 				)}
--- github-glazer/tailwind.config.js
+++ github-glazer/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
 	content: ["./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				poppins: ["Poppins", "sans-serif"],
+			},
+		},
 	},
 	plugins: [require("daisyui")],
 	daisyui: {
